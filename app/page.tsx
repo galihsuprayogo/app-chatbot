9f37288d1e64b70ad6e43ef6ce42eee25da50cd1
Galih Suprayogo <suprayogogalih@gmail.com>
2024-02-22 10:38:22 +0700

    Add chatbot iframe

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,23 @@
 import { Chat, Send } from '@/components'
 
+// const NativeChatBot = () => (
+//   <div className='flex h-screen flex-col'>
+//     <div className='flex-1 flex-grow overflow-y-scroll bg-purple-200 px-5 py-6 md:px-10'>
+//       <Chat method='get' avatar_url='2.png' className='ring-white' />
+//       <Chat method='post' avatar_url='6.png' className='ring-blue-500' />
+//     </div>
+//     <Send />
+//   </div>
+// )
+
 export default function RootMain() {
   return (
-    <div className='flex h-screen flex-col'>
-      <div className='flex-1 flex-grow overflow-y-scroll bg-purple-200 px-5 py-6 md:px-10'>
-        <Chat method='get' avatar_url='2.png' className='ring-white' />
-        <Chat method='post' avatar_url='6.png' className='ring-blue-500' />
-      </div>
-      <Send />
+    <div className='flex h-screen flex-col bg-none'>
+      <iframe
+        src='https://www.chatbase.co/chatbot-iframe/HPTudsPpq7XmNhucKTlWY'
+        width='100%'
+        className='h-full min-h-[700px]'
+      ></iframe>
     </div>
   )
 }
